34645bd752bea68b5c0ab5567d22b1a9bd931dde
enabia1004 <kasarbia1004@naver.com>
2025-06-17 14:49:05 +0900

    Add files via upload

---
+++ 02_select&css/js/main.js
@@ -0,0 +1,50 @@
+// 제이쿼리를 사용할 준비작업(바인딩)
+$(function(){
+    // 제이쿼리 프로그램 실행
+    // $("선택자").명령함수()
+    // 명령함수 css() -> jquery프로그램으로 css접근할때 사용
+    
+    $("h1").css({"color":"red"});
+    $(".date").css({"font-size":"80px","color":"pink","background":"purple"});
+    $(".para").css({"font-size":"10px","border":"10px solid blue"})
+    $("ul li:last-child").css({"font-size":48})
+    $("ul li").css({"border":"1px solid #AA2327"})
+    $("a").css({"font-size":15,"border":"5px solid blue","padding":11})
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
