c6b051173568285d3cb59957eabaff4a8eb3ac0d
enabia1004 <kasarbia1004@naver.com>
2025-06-23 16:29:42 +0900

    Add files via upload

---
+++ 10_bannerslide/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+$(".list").slick({
+    autoplay:true,
+    // 한화면에 스크롤보일 개수
+    slidesToShow:2,
+    // 스크롤할때 얼마나 나가는지 설정
+    slidesToScroll:1,
+    // 좌우버튼숨기기
+    arrows:false,
+    // 페이지 버튼 보이기
+    dots:true
+});
+});
