eb9da635a34cb2da7921ab2552c08de2dd29da5f
enabia1004 <kasarbia1004@naver.com>
2025-07-01 13:45:54 +0900

    Add files via upload

--- 17_portfoliolntro/js/main.js
+++ 17_portfoliolntro/js/main.js
@@ -1,17 +1,32 @@
 // let ppp = document.getElementsByClassName(".webhour")
 // console.log(ppp)
-
 function time(){
     // DOM객체를 선택해서 변수에 저장
     let webh = document.querySelector(".webhour")
     let webm = document.querySelector(".webmin");
     let webs = document.querySelector(".websec");
     let webe = document.querySelector(".webmsec")
-    console.log(webh,webm,webs,webe)
+    console.log(webh,webm,webs,webe,)
+    let yor = document.querySelector(".ye")
+    let tor = document.querySelector(".te")
+    let por = document.querySelector(".pe")
 
     // 날짜/시간 객체를 생성
     let date = new Date();
     console.log(date)
+
+    
+    let year = date.getFullYear();
+    console.log(year);
+    yor.innerHTML = year;
+    let mon = date.getMonth() + 1;
+    console.log(mon)
+    tor.innerHTML = mon
+    let da = date.getDate();
+    console.log(da)
+    por.innerHTML = da
+
+
     // 시간
     let hour = date.getHours();
     console.log(hour)
@@ -42,11 +57,37 @@ function time(){
             return x;
         }
     }
-
-
-
 }
 // 매 1초마다 time함수를 반복실행
 setInterval(function(){
     time();
 },100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
