9bb2219a71b12bf597b7fdf6a470fc9df19c5973
enabia1004 <kasarbia1004@naver.com>
2025-07-01 16:42:25 +0900

    Add files via upload

--- 14_tap/js/main.js
+++ 14_tap/js/main.js
@@ -1,6 +1,10 @@
 $(function(){
-            $(".section .inner .right .wrap .menu li").click(function(){
-                $(".section .inner .right .wrap .menu li").removeClass("on")
-                $(this).addClass("on")
-            })
-        })
+    $(".product2").hide();
+    $("ul.menu li").click(function(){
+        // 선택한 요소가 몇번째가 선택
+        let num = $(this).index()
+        console.log(num);
+        $(".list").hide();
+        $(".list").eq(num).show()
+    })
+})
