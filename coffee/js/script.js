d7c9dd06dd5b0fb45233aa265f5cc42d984d008f
enabia1004 <kasarbia1004@naver.com>
2025-07-22 10:13:00 +0900

    Add files via upload

--- coffee/js/script.js
+++ coffee/js/script.js
@@ -27,6 +27,41 @@ $(document).ready(function(){
     });
 });
 
+// 배너슬라이드
+$(document).ready(function(){
+    $('.bannerslide').slick({
+        // 자동실행
+        autoplay:true,
+        // 좌우버튼
+        arrows:false,
+        // 페이지버튼
+        dots:true
+    });
+});
+
+// tab기능
+// tab li를 클릭하면 해당 li에 on class추가
+// 나머진 on class제거
+$(".tab li").click(function(e){
+    // 위로 올라가는 기능 방지
+    e.preventDefault()
+    $(".tab li").removeClass("on");
+    $(this).addClass("on")
+})
+
+// list박스를 클릭하면 on class추가/삭제
+$(".content .list li").click(function(e){
+    e.preventDefault()
+    $(".content .list li").removeClass("on")
+    $(this).addClass("on")
+})
+
+
+
+
+
+
+
 
 
 
